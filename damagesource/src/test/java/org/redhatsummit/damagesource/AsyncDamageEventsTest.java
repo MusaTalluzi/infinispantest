a8631c61b7e7c60dc2fd10c721e3dc657c6b4a6b
Musa Talluzi <mtalluzi@redhat.com>
2019-03-07 16:36:03 -0500

    Produce SocketTimeoutException error

--- damagesource/src/test/java/org/redhatsummit/damagesource/AsyncDamageEventsTest.java
+++ damagesource/src/test/java/org/redhatsummit/damagesource/AsyncDamageEventsTest.java
@@ -6,8 +6,11 @@ import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.Callable;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Logger;
@@ -20,6 +23,7 @@ import org.infinispan.counter.api.CounterManager;
 import org.infinispan.counter.api.CounterState;
 import org.infinispan.counter.api.CounterType;
 import org.infinispan.counter.api.StrongCounter;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -38,7 +42,7 @@ public class AsyncDamageEventsTest {
 
     private static final String HEALTH_COUNTER_NAME = "health.counter.name";
     private static final int NUM_THREADS = 1;
-    private static final int NUM_DECREMENTS_PER_THREAD = 4;
+    private static final int NUM_DECREMENTS_PER_THREAD = 10;
     private static final String HEALTH_COUNTER_INITIAL_VALUE = "health.counter.initial.value";
     private static final String HEALTH_COUNTER_LOWER_BOUND = "health.counter.lower.bound";
 
@@ -47,8 +51,19 @@ public class AsyncDamageEventsTest {
     private AtomicInteger numAttemptedDecrements = new AtomicInteger(0);
     private AtomicInteger numSuccessfulDecrements = new AtomicInteger(0);
     private AtomicInteger numNonSuccessfulDecrements = new AtomicInteger(0);
+    private Properties props = new Properties();
 
     private ExecutorService executorService = Executors.newFixedThreadPool(NUM_THREADS);
+    private ScheduledExecutorService resetExecutorService = Executors.newSingleThreadScheduledExecutor();
+
+    @Before
+    public void init() {
+        try {
+            props.load(AsyncDamageEventsTest.class.getClassLoader().getResourceAsStream("infinispan.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Test
     public void decrementCounterAsync() {
@@ -67,18 +82,22 @@ public class AsyncDamageEventsTest {
         counterManager.defineCounter(counterName, cc);
         StrongCounter healthCounter = counterManager.getStrongCounter(counterName);
 
-        // Start OptaPlanner thread to listen to counter decrements
-        OptaPlannerThread optaPlannerThread = new OptaPlannerThread(healthCounter);
-        optaPlannerThread.setDaemon(true);
+        // Start Listener thread to listen to counter decrements
+        ListenerThread listenerThread = new ListenerThread(healthCounter);
+        listenerThread.setDaemon(true);
+        listenerThread.setPriority(Thread.MAX_PRIORITY);
 
-        optaPlannerThread.start();
+        listenerThread.start();
         try {
-            startLatch.await(); // Wait until OptaPlanner thread register a listener
+            startLatch.await(); // Wait until Listener thread register a listener
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        assertEquals(0, optaPlannerThread.getNumDecrementEvents());
-        assertEquals(0, optaPlannerThread.getNumResets());
+        assertEquals(0, listenerThread.getNumDecrementEvents());
+        assertEquals(0, listenerThread.getNumResets());
+
+        // Reset counter every second:
+//        resetExecutorService.scheduleAtFixedRate(healthCounter::reset, 1, 1, TimeUnit.SECONDS);
 
         // Start threads to submit damage events
         List<ThreadDecrementTask> threadDecrementTaskList = new ArrayList<>(NUM_THREADS);
@@ -86,9 +105,27 @@ public class AsyncDamageEventsTest {
             threadDecrementTaskList.add(new ThreadDecrementTask(healthCounter));
         }
         try {
-            executorService.invokeAll(threadDecrementTaskList,
-                    Math.max(1000, NUM_THREADS * NUM_DECREMENTS_PER_THREAD), TimeUnit.MILLISECONDS);
-            Thread.sleep(Math.max(1000, NUM_DECREMENTS_PER_THREAD * NUM_THREADS)); // Give listener thread time to handleEvents
+            executorService.invokeAll(threadDecrementTaskList)
+                    .parallelStream()
+                    .flatMap(listFuture -> {
+                        try {
+                            return listFuture.get().parallelStream();
+                        } catch (InterruptedException | ExecutionException e) {
+                            throw new RuntimeException(e);
+                        }
+                    })
+                    .forEach(integerFuture -> {
+                        try {
+                            if (integerFuture.get() == 1) {
+                                numSuccessfulDecrements.incrementAndGet();
+                            } else {
+                                numNonSuccessfulDecrements.incrementAndGet();
+                            }
+                        } catch (InterruptedException | ExecutionException e) {
+                            throw new RuntimeException(e);
+                        }
+                    });
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -96,28 +133,28 @@ public class AsyncDamageEventsTest {
         LOGGER.info("Number of attempted decrements: " + numAttemptedDecrements.get());
         LOGGER.info("Number of successful decrements: " + numSuccessfulDecrements.get());
         LOGGER.info("Number of non-successful decrements: " + numNonSuccessfulDecrements.get());
-        LOGGER.info(String.format("Number of OptaPlanner successfulDecrements: %d, resets: %d.",
-                optaPlannerThread.getNumDecrementEvents(), optaPlannerThread.getNumResets()));
+        LOGGER.info(String.format("Number of listener successfulDecrements: %d, resets: %d.",
+                listenerThread.getNumDecrementEvents(), listenerThread.getNumResets()));
+        healthCounter.getValue().thenAccept(value -> LOGGER.info("Counter final value is: " + value));
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
-        assertEquals(numAttemptedDecrements.get(), numSuccessfulDecrements.get() + numNonSuccessfulDecrements.get());
-        assertEquals(numSuccessfulDecrements.get(), optaPlannerThread.getNumDecrementEvents());
-        assertEquals(NUM_THREADS * NUM_DECREMENTS_PER_THREAD, numAttemptedDecrements.get());
-        int expectedNumResets = Math.floorDiv(
-                numSuccessfulDecrements.get(), Integer.parseInt(infinispanProperty(HEALTH_COUNTER_INITIAL_VALUE)));
-        assertEquals(expectedNumResets, optaPlannerThread.getNumResets());
+//        assertEquals(numAttemptedDecrements.get(), numSuccessfulDecrements.get() + numNonSuccessfulDecrements.get());
+//        assertEquals(numSuccessfulDecrements.get(), listenerThread.getNumDecrementEvents());
+//        int expectedNumResets = Math.floorDiv(
+//                numSuccessfulDecrements.get(), Integer.parseInt(infinispanProperty(HEALTH_COUNTER_INITIAL_VALUE)));
+//        assertEquals(expectedNumResets, listenerThread.getNumResets());
+//        assertEquals(NUM_THREADS * NUM_DECREMENTS_PER_THREAD, numAttemptedDecrements.get());
     }
 
     private String infinispanProperty(String name) {
-        Properties props = new Properties();
-        try {
-            props.load(AsyncDamageEventsTest.class.getClassLoader().getResourceAsStream("infinispan.properties"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
         return props.getProperty(name);
     }
 
-    private class ThreadDecrementTask implements Callable<Boolean> {
+    private class ThreadDecrementTask implements Callable<List<Future<Integer>>> {
 
         private StrongCounter strongCounter;
 
@@ -126,25 +163,28 @@ public class AsyncDamageEventsTest {
         }
 
         @Override
-        public Boolean call() {
+        public List<Future<Integer>> call() throws InterruptedException {
+            List<Future<Integer>> decrementFutureList = new ArrayList<>(NUM_DECREMENTS_PER_THREAD);
             for (int i = 0; i < NUM_DECREMENTS_PER_THREAD; i++) {
-                strongCounter.decrementAndGet()
-                        .thenRun(() -> {
+                decrementFutureList.add(strongCounter.decrementAndGet()
+                        .handle((aLong, throwable) -> {
                             numAttemptedDecrements.incrementAndGet();
-                            numSuccessfulDecrements.incrementAndGet();
-                        })
-                        .exceptionally(throwable -> {
-                            numAttemptedDecrements.incrementAndGet();
+                            LOGGER.info("Handling... by " + Thread.currentThread());
+                            if (throwable == null) {
+                                return 1;
+                            }
                             LOGGER.warning(throwable.toString());
-                            numNonSuccessfulDecrements.incrementAndGet();
-                            return null;
-                        });
+                            return 0;
+                        }));
+                if (i % Integer.parseInt(infinispanProperty(HEALTH_COUNTER_INITIAL_VALUE)) == 0) { // Give listener thread a chance to reset counter
+//                    Thread.sleep(100); // TODO: uncomment this when testing resetting
+                }
             }
-            return true;
+            return decrementFutureList;
         }
     }
 
-    private class OptaPlannerThread extends Thread {
+    private class ListenerThread extends Thread {
 
         private final long healthCounterLowerBound = Long.parseLong(infinispanProperty(HEALTH_COUNTER_LOWER_BOUND));
 
@@ -152,7 +192,7 @@ public class AsyncDamageEventsTest {
         private AtomicInteger numResets = new AtomicInteger(0);
         private AtomicInteger numDecrementEvents = new AtomicInteger(0);
 
-        public OptaPlannerThread(StrongCounter strongCounter) {
+        public ListenerThread(StrongCounter strongCounter) {
             this.strongCounter = strongCounter;
         }
 
@@ -171,11 +211,14 @@ public class AsyncDamageEventsTest {
                 if (counterEvent.getNewValue() < counterEvent.getOldValue()) { // only count decrement events
                     numDecrementEvents.incrementAndGet();
                 }
-                if (counterEvent.getNewValue() == healthCounterLowerBound
-                        && counterEvent.getNewState().equals(CounterState.VALID)) {
-                    strongCounter.sync().reset();
+                if (counterEvent.getNewValue() > counterEvent.getOldValue()) { // another thread reset the counter
                     numResets.incrementAndGet();
                 }
+//                if (counterEvent.getNewValue() == healthCounterLowerBound
+//                        && counterEvent.getNewState().equals(CounterState.VALID)) { // first time to reach lower_bound, reset counter
+//                    strongCounter.reset();
+//                    numResets.incrementAndGet();
+//                }
             });
             startLatch.countDown();
         }
